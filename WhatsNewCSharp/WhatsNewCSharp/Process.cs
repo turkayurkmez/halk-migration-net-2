f2d08cb4006ca942fa7c47e23c047047a23708d7
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-12 11:11:16 +0300

    What's new C# 10?

---
+++ WhatsNewCSharp/WhatsNewCSharp/Process.cs
@@ -0,0 +1,13 @@
+﻿
+//File-scoped namespace: Dosya adı ve konumu üzerinden namespace'a dahil edilir.
+namespace WhatsNewCSharp;
+
+public class Process
+{
+    public void WriteToStream(string path)
+    {
+        DataTable dt = new DataTable();
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+
+    }
+}
---
+++ WhatsNewCSharp/WhatsNewCSharp/global-usings.cs
@@ -0,0 +1,4 @@
+﻿global using System.Data;
+global using System.IO;
+global using System.IO.Pipes;
+
